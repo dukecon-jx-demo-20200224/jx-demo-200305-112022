487290c480837f7c1e9d53be878ca8af63f66f50
Gerd Aschemann <gerd@aschemann.net>
2020-03-05 11:20:33 +0000

    Initial import

---
+++ src/test/java/org/dukecon/jx/demo/jxdemo200305112022/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dukecon.jx.demo.jxdemo200305112022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
